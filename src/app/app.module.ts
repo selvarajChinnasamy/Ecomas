39afe16dcd5ed8c9026f0d7f44c6d71caedc8272
Selvaraj <selvaraj.infinijith@gmail.com>
2018-01-19 20:00:08 +0530

    commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './slider/slider.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SliderComponent,
+    ProductsComponent,
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
